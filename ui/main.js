70702b742b357403ec82d947a92ca6f5d403978b
AbhinavAn-and <abhinav.aa10@gmail.com>
2017-09-15 23:49:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,16 @@
 console.log('Loaded!');
+//Change the style of main-text div
 var element= document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
+
+// Move the image
+var img=document.getElementById('madi');
+var marginleft=0;
+ function moveright(){
+     marginleft=marginleft+10;
+     img.style.marginLeft=marginLeft+'px';
+ }
+img.onclick=function(){
+    var interval=setInterval(moveright,100);
+}
+
